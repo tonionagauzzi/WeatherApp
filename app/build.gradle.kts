0623bbb17b19ec6d903361cd8e45cc574778ac6f
tonionagauzzi <vitantonionagauzzi@gmail.com>
2025-05-05 01:18:33 +0900

    androidx.compose.ui:uiを消す

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,7 +49,6 @@ dependencies {
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.material3)
-    implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
 
